a8f7305a75d7d3305b52d8743a08f40b152a7702
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-18 18:06:07 -0300

    Other entities and second migration

--- SistemaWeb/Migrations/20190418210435_OtherEntities.Designer.cs
+++ SistemaWeb/Migrations/20190418210435_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SistemaWeb.Models;
 
 namespace SistemaWeb.Migrations
 {
     [DbContext(typeof(SistemaWebContext))]
-    partial class SistemaWebContextModelSnapshot : ModelSnapshot
+    [Migration("20190418210435_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
