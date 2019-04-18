f49a61439709a546a753ff0f639fc906d55ee4f9
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-18 18:34:41 -0300

    Seeding service

--- SistemaWeb/Startup.cs
+++ SistemaWeb/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SistemaWeb.Models;
+using SistemaWeb.Data;
 
 namespace SistemaWeb
 {
@@ -38,15 +39,18 @@ namespace SistemaWeb
 
             services.AddDbContext<SistemaWebContext>(options =>
                    options.UseMySql(Configuration.GetConnectionString("SistemaWebContext"), builder =>
- builder.MigrationsAssembly("SistemaWeb")));
+                    builder.MigrationsAssembly("SistemaWeb")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
