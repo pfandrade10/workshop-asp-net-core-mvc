f34374f199e37bf9e88eede284c365c71947d22d
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-18 17:22:23 -0300

    MySQL adaptation and first migration

--- SistemaWeb/Startup.cs
+++ SistemaWeb/Startup.cs
@@ -37,7 +37,8 @@ namespace SistemaWeb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SistemaWebContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SistemaWebContext")));
+                   options.UseMySql(Configuration.GetConnectionString("SistemaWebContext"), builder =>
+ builder.MigrationsAssembly("SistemaWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
