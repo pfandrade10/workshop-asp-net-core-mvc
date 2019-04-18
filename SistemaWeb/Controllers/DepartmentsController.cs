8a42bc5d15ed74df330577a8e1f5632108f5abcf
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-18 15:13:38 -0300

    First Model-Controller-View - Department

---
+++ SistemaWeb/Controllers/DepartmentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaWeb.Models;
+
+namespace SistemaWeb.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletrocnics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
--- SistemaWeb/Controllers/HomeController.cs
+++ SistemaWeb/Controllers/HomeController.cs
@@ -38,7 +38,7 @@ namespace SistemaWeb.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new Models.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
