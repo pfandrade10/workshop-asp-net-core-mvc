c43a93a6d79be5aa9062b5a69c41f409de0aec7d
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-18 14:12:56 -0300

    First controller and Razor pages tests

--- SistemaWeb/Controllers/HomeController.cs
+++ SistemaWeb/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SistemaWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course.";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
