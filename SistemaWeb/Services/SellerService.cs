d650f531b43947f3eafe77b4d486ee6d7d54056e
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-19 18:08:54 -0300

    Seller details and eager loading

--- SistemaWeb/Services/SellerService.cs
+++ SistemaWeb/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SistemaWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace SistemaWeb.Services
@@ -29,7 +30,7 @@ namespace SistemaWeb.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
