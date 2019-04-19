b643c3aa65a1579bc8a1b446833ea73ee4ba02b2
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-19 14:15:49 -0300

    SellerService and basic FindAll

--- SistemaWeb/Controllers/SellersController.cs
+++ SistemaWeb/Controllers/SellersController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SistemaWeb.Services;
 
 namespace SistemaWeb.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _sellerService.FindAll();
+            return View(list);
         }
+
+
     }
 }
--- SistemaWeb/Startup.cs
+++ SistemaWeb/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SistemaWeb.Models;
 using SistemaWeb.Data;
+using SistemaWeb.Services;
 
 namespace SistemaWeb
 {
@@ -42,6 +43,7 @@ namespace SistemaWeb
                     builder.MigrationsAssembly("SistemaWeb")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
