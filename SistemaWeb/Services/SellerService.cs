0a6683885949bd1959fe5d692ed98f3b0ff986e2
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-19 15:58:58 -0300

    Foreign key not null (referential integrity)

--- SistemaWeb/Services/SellerService.cs
+++ SistemaWeb/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace SistemaWeb.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
