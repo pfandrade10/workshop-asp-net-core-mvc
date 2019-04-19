212747aee0da859ac037a2d59e9624b20dc1c8e5
Pedro Ferraz de Andrade <pfandrade10@hotmail.com>
2019-04-19 18:02:32 -0300

    Delete seller action

--- SistemaWeb/Controllers/SellersController.cs
+++ SistemaWeb/Controllers/SellersController.cs
@@ -40,5 +40,41 @@ namespace SistemaWeb.Controllers
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
     }
 }
