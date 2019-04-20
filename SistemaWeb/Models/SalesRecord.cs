d1e63535ae07ff21dd7c4c07f029f4762f86cbf3
Pedro Andrade <pfandrade10@hotmail.com>
2019-04-20 18:20:05 -0300

    Implementing grouping search

--- SistemaWeb/Models/SalesRecord.cs
+++ SistemaWeb/Models/SalesRecord.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using SistemaWeb.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace SistemaWeb.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+
         public SaleStatus Status { get; set; }
         public Seller Seller { get; set; }
 
